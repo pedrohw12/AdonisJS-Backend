5fc37a287ff51d5bcee6d367a1951631cc837824
Pedro Wanderley <peu-zico>
2019-12-18 15:28:05 -0300

    beginning routes authorization

--- start/app.js
+++ start/app.js
@@ -1,15 +1,5 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Providers
-|--------------------------------------------------------------------------
-|
-| Providers are building blocks for your Adonis app. Anytime you install
-| a new Adonis specific package, chances are you will register the
-| provider here.
-|
-*/
 const providers = [
   '@adonisjs/framework/providers/AppProvider',
   '@adonisjs/auth/providers/AuthProvider',
@@ -24,46 +14,17 @@ const providers = [
   'adonis-acl/providers/AclProvider'
 ]
 
-/*
-|--------------------------------------------------------------------------
-| Ace Providers
-|--------------------------------------------------------------------------
-|
-| Ace providers are required only when running ace commands. For example
-| Providers for migrations, tests etc.
-|
-*/
 const aceProviders = [
   '@adonisjs/lucid/providers/MigrationsProvider',
   'adonis-kue/providers/CommandsProvider',
   'adonis-acl/providers/CommandsProvider'
 ]
 
-/*
-|--------------------------------------------------------------------------
-| Aliases
-|--------------------------------------------------------------------------
-|
-| Aliases are short unique names for IoC container bindings. You are free
-| to create your own aliases.
-|
-| For example:
-|   { Route: 'Adonis/Src/Route' }
-|
-*/
 const aliases = {
   Role: 'Adonis/Acl/Role',
   Permission: 'Adonis/Acl/Permission'
 }
 
-/*
-|--------------------------------------------------------------------------
-| Commands
-|--------------------------------------------------------------------------
-|
-| Here you store ace commands for your package
-|
-*/
 const commands = []
 
 const jobs = ['App/Jobs/InvitationEmail']
