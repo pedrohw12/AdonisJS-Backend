909f8a785032fe6ce4d5e565c1d9d6b0b394604d
Pedro Wanderley <peu-zico>
2019-12-17 22:25:44 -0300

    email with adonis and redis

--- start/app.js
+++ start/app.js
@@ -16,7 +16,10 @@ const providers = [
   '@adonisjs/bodyparser/providers/BodyParserProvider',
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/lucid/providers/LucidProvider',
-  '@adonisjs/lucid-slugify/providers/SlugifyProvider'
+  '@adonisjs/lucid-slugify/providers/SlugifyProvider',
+  'adonis-kue/providers/KueProvider',
+  '@adonisjs/redis/providers/RedisProvider',
+  '@adonisjs/mail/providers/MailProvider'
 ]
 
 /*
@@ -29,7 +32,8 @@ const providers = [
 |
 */
 const aceProviders = [
-  '@adonisjs/lucid/providers/MigrationsProvider'
+  '@adonisjs/lucid/providers/MigrationsProvider',
+  'adonis-kue/providers/CommandsProvider'
 ]
 
 /*
@@ -56,4 +60,6 @@ const aliases = {}
 */
 const commands = []
 
-module.exports = { providers, aceProviders, aliases, commands }
+const jobs = ['App/Jobs/InvitationEmail']
+
+module.exports = { providers, aceProviders, aliases, commands, jobs }
